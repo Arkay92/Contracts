fd20afff44a2947908ef0b50e86252e592e14cf3
Robert McMenemy <55621684+Arkay92@users.noreply.github.com>
2023-05-28 22:34:33 +0100

    Update SoftwareLicense.sol

--- SoftwareLicense.sol
+++ SoftwareLicense.sol
@@ -12,54 +12,54 @@ import "@openzeppelin/contracts/utils/Base64.sol";
 
 contract SoftwareLicense is ERC721A, Ownable, ReentrancyGuard {
 	// Use strings for uints to allow easier conversion to string
-    using Strings for uint256;
+	using Strings for uint256;
 
-    // Struct to hold info about a token
-    struct TokenInfo {
-        uint256 timeMinted;
-        uint256 subscriptionLength;
-        uint256 lastRenewal;
-        string status;
-    }
+	// Struct to hold info about a token
+	struct TokenInfo {
+	uint256 timeMinted;
+	uint256 subscriptionLength;
+	uint256 lastRenewal;
+	string status;
+	}
 
-    // Set starter variables
-    bool public mintStarted = false;
-    uint256 trialLength = 7 days;
-    uint256 public licenceCost = 0.15 ether;
+	// Set starter variables
+	bool public mintStarted = false;
+	uint256 trialLength = 7 days;
+	uint256 public licenceCost = 0.15 ether;
 
-    // Mapping to hold info for each token id
-    mapping(uint256 => TokenInfo) private tokenData;
+	// Mapping to hold info for each token id
+	mapping(uint256 => TokenInfo) private tokenData;
 
-    // Mapping to hold token against user
-    mapping(address => uint256) private tokenHoldings;
+	// Mapping to hold token against user
+	mapping(address => uint256) private tokenHoldings;
 
-    // Token name and ticker set in constructor params
-    constructor() ERC721A("Software License: ACME CO.", "SLCN") { }
+	// Token name and ticker set in constructor params
+	constructor() ERC721A("Software License: ACME CO.", "SLCN") { }
 
-     // Dev mint 
-    function devMint(uint8 quantity, string memory status) external onlyOwner {
-        uint256 i = totalSupply();
-        uint256 newQuantity = i + quantity;
+	// Dev mint 
+	function devMint(uint8 quantity, string memory status) external onlyOwner {
+		uint256 i = totalSupply();
+		uint256 newQuantity = i + quantity;
 
-        _mint(msg.sender, quantity);    
+		_mint(msg.sender, quantity);    
 
-        do {
-            setTokenInfo(i, status);
+		do {
+		    setTokenInfo(i, status);
 
-            ++i;
-        } while (i <= newQuantity);
-    }
+		    ++i;
+		} while (i <= newQuantity);
+	}
 
-    // Public mint
-    function mint() external payable nonReentrant {
-        uint256 nextID = totalSupply() + 1;
+	// Public mint
+	function mint() external payable nonReentrant {
+		uint256 nextID = totalSupply() + 1;
 
-        _mint(msg.sender, 1);   
+		_mint(msg.sender, 1);   
 
-        setTokenInfo(nextID, "Trial");
+		setTokenInfo(nextID, "Trial");
 
-        tokenHoldings[msg.sender] = nextID;
-    }
+		tokenHoldings[msg.sender] = nextID;
+	}
 
 	// Override _startTokenId() function of ERC721A
 	function _startTokenId() internal view virtual override returns (uint256) {
@@ -68,58 +68,59 @@ contract SoftwareLicense is ERC721A, Ownable, ReentrancyGuard {
 
 	// Override tokenURI() function of ERC721A with custom on chain metadata
 	function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
-        bytes memory dataURI = abi.encodePacked(
-            '{',
-                '"description": "A license for ACME Co. Software",', 
-                '"external_url": "https://acmeco.com",', 
-                '"image": "https://bafkreifh6hd2fv7fi5rtczovbjc7gefvvr3qbafnavufi5r36sevrmnvn4.ipfs.nftstorage.link/",', 
-                '"name": "ACME Co. License",',
-                '"attributes": [',
-                    '{ "Status":', tokenData[tokenId].status, '},',
-                    '{ "Time Minted":', tokenData[tokenId].timeMinted, '},',
-                    '{ "Subscription Length":', tokenData[tokenId].subscriptionLength, '}',
-                    '{ "Last Renewal":', tokenData[tokenId].lastRenewal, '}'
-                ']',
-            '}'
-        );
-        return string(
-            abi.encodePacked(
-                "data:application/json;base64,",
-                Base64.encode(dataURI)
-            )
-        );
+		bytes memory dataURI = abi.encodePacked(
+		    '{',
+			'"description": "A license for ACME Co. Software",', 
+			'"external_url": "https://acmeco.com",', 
+			'"image": "https://bafkreifh6hd2fv7fi5rtczovbjc7gefvvr3qbafnavufi5r36sevrmnvn4.ipfs.nftstorage.link/",', 
+			'"name": "ACME Co. License",',
+			'"attributes": [',
+			    '{ "Status":', tokenData[tokenId].status, '},',
+			    '{ "Time Minted":', tokenData[tokenId].timeMinted, '},',
+			    '{ "Subscription Length":', tokenData[tokenId].subscriptionLength, '}',
+			    '{ "Last Renewal":', tokenData[tokenId].lastRenewal, '}'
+			']',
+		    '}'
+		);
+		return string(
+		    abi.encodePacked(
+			"data:application/json;base64,",
+			Base64.encode(dataURI)
+		    )
+		);
 	}
 
 	// Helper function to set token info
-    function setTokenInfo(uint256 tokenID, string memory status) internal {
-        tokenData[tokenID].timeMinted = block.timestamp;
-        tokenData[tokenID].lastRenewal = block.timestamp;
-        tokenData[tokenID].status = status;
+	function setTokenInfo(uint256 tokenID, string memory status) internal {
+		tokenData[tokenID].timeMinted = block.timestamp;
+		tokenData[tokenID].lastRenewal = block.timestamp;
+		tokenData[tokenID].status = status;
 
-        if(keccak256(abi.encodePacked(status)) == keccak256(abi.encodePacked("Paid"))) tokenData[tokenID].subscriptionLength = 365 days;
-        if(keccak256(abi.encodePacked(status)) == keccak256(abi.encodePacked("Trial"))) tokenData[tokenID].subscriptionLength = trialLength;
-    }
+		if(keccak256(abi.encodePacked(status)) == keccak256(abi.encodePacked("Paid"))) tokenData[tokenID].subscriptionLength = 365 days;
+		if(keccak256(abi.encodePacked(status)) == keccak256(abi.encodePacked("Trial"))) tokenData[tokenID].subscriptionLength = trialLength;
+	}
 
-    // Function to let license holders buy subscriptions from their trial tokens
-    function buyLicense() external payable {
-        tokenData[tokenHoldings[msg.sender]].status = "Paid";
-        tokenData[tokenHoldings[msg.sender]].subscriptionLength = 365 days;
-    }
+	// Function to let license holders buy subscriptions from their trial tokens
+	function buyLicense() external payable {
+	    require(msg.value >= licenceCost, "[Error] Please send the correct amount of ETH for the license cost");
+	    tokenData[tokenHoldings[msg.sender]].status = "Paid";
+	    tokenData[tokenHoldings[msg.sender]].subscriptionLength = 365 days;
+	}
 
-    // Allow owner to toggle mint status of trials
-    function toggleMintStatus() external onlyOwner {
-        mintStarted = !mintStarted;
-    }
+	// Allow owner to toggle mint status of trials
+	function toggleMintStatus() external onlyOwner {
+		mintStarted = !mintStarted;
+	}
 
-    // Allow owner to change price of full license
-    function setLicenseCost(uint256 value) external onlyOwner {
-        licenceCost = value;
-    }
+	// Allow owner to change price of full license
+	function setLicenseCost(uint256 value) external onlyOwner {
+		licenceCost = value;
+	}
 
-    // Function to return token id the user holds
-    function getUserToken(address user) external view onlyOwner returns(TokenInfo memory) {
-        return tokenData[tokenHoldings[user]];
-    }
+	// Function to return token id the user holds
+	function getUserToken(address user) external view onlyOwner returns(TokenInfo memory) {
+		return tokenData[tokenHoldings[user]];
+	}
 
 	// Function to send funds to owner
 	function sendFunds(uint256 _totalMsgValue) public payable {
@@ -142,20 +143,20 @@ contract SoftwareLicense is ERC721A, Ownable, ReentrancyGuard {
 		sendFunds(address(this).balance);
 	}
 
-    // Check user only holds one token
-    // Check token is being sold if it is then only allow active tokens to be transferred
-    // If token is passed its active period deactivate it
-    function transferFrom(address from, address to, uint256 tokenId) public payable virtual override {
-        if(from != address(owner()) && from != address(this) && to != address(owner())  && to != address(this)) {
-            require(balanceOf(to)< 1, "[Error] User can only hold once license");
-            if(msg.value > 0) {
-                if(block.timestamp > tokenData[tokenId].lastRenewal + tokenData[tokenId].subscriptionLength) {
-                    tokenData[tokenId].status = "Deactivated";
-                    tokenData[tokenId].subscriptionLength = 0 days;
-                }
-                require(keccak256(abi.encodePacked(tokenData[tokenId].status)) == keccak256(abi.encodePacked("Paid")), "[Error] Only active tokens can be sold");
-            }
-        }
-        super.transferFrom(from, to, tokenId);
-    }
+	// Check user only holds one token
+	// Check token is being sold if it is then only allow active tokens to be transferred
+	// If token is passed its active period deactivate it
+	function transferFrom(address from, address to, uint256 tokenId) public payable virtual override {
+		if(from != address(owner()) && from != address(this) && to != address(owner())  && to != address(this)) {
+		    require(balanceOf(to)< 1, "[Error] User can only hold once license");
+		    if(msg.value > 0) {
+			if(block.timestamp > tokenData[tokenId].lastRenewal + tokenData[tokenId].subscriptionLength) {
+			    tokenData[tokenId].status = "Deactivated";
+			    tokenData[tokenId].subscriptionLength = 0 days;
+			}
+			require(keccak256(abi.encodePacked(tokenData[tokenId].status)) == keccak256(abi.encodePacked("Paid")), "[Error] Only active tokens can be sold");
+		    }
+		}
+		super.transferFrom(from, to, tokenId);
+	}
 }
