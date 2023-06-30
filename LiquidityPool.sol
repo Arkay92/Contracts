eda72668d8be13a675d779eb8c9a42ded958bb59
Robert McMenemy <55621684+Arkay92@users.noreply.github.com>
2023-06-30 21:57:09 +0100

    Update LiquidityPool.sol

--- LiquidityPool.sol
+++ LiquidityPool.sol
@@ -4,19 +4,23 @@ pragma solidity ^0.8.8;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./BankReserve.sol";
 
 contract LiquidityPool is Ownable, ReentrancyGuard {
     AggregatorV3Interface public goldOracle;
     AggregatorV3Interface public silverOracle;
-    AggregatorV3Interface public copperOracle;
 
     uint256 public goldReserve;
     uint256 public silverReserve;
-    uint256 public copperReserve;
 
-    constructor() {
+    mapping(uint256 => bool) public depositedNFTs;
+
+    BankReserve public bankReserve;
+
+    constructor(address _bankReserve) {
         goldOracle = AggregatorV3Interface(0x214eD9Da11D2fbe465a6fc601a91E62EbEc1a0D6);
         silverOracle = AggregatorV3Interface(0x379589227b15F1a12195D3f2d90bBc9F31f95235);
+        bankReserve = BankReserve(_bankReserve);
     }
 
     function getGoldPrice() public view returns (uint256) {
@@ -31,32 +35,25 @@ contract LiquidityPool is Ownable, ReentrancyGuard {
         return uint256(price);
     }
 
-    function depositGold(uint256 _amount) external onlyOwner nonReentrant {
-        require(_amount > 0, "Invalid amount");
-        goldReserve += _amount;
-    }
-
-    function depositSilver(uint256 _amount) external onlyOwner nonReentrant {
-        require(_amount > 0, "Invalid amount");
-        silverReserve += _amount;
-    }
-
-    function withdrawGold(uint256 _amount) external onlyOwner nonReentrant {
-        require(_amount > 0, "Invalid amount");
-        require(_amount <= goldReserve, "Insufficient gold reserves");
-        goldReserve -= _amount;
-    }
-
-    function withdrawSilver(uint256 _amount) external onlyOwner nonReentrant {
-        require(_amount > 0, "Invalid amount");
-        require(_amount <= silverReserve, "Insufficient silver reserves");
-        silverReserve -= _amount;
-    }
-
     function getExchangeRate() public view returns (uint256) {
         uint256 goldPrice = getGoldPrice();
         uint256 silverPrice = getSilverPrice();
-        
+
         return (goldPrice * goldReserve + silverPrice * silverReserve) / (goldReserve + silverReserve);
+    }
+
+    function depositNFT(uint256 _tokenId) external nonReentrant {
+        require(!depositedNFTs[_tokenId], "NFT already deposited");
+        (, , uint256 weight, ) = bankReserve.bullionData(_tokenId);
+        goldReserve += weight;
+        depositedNFTs[_tokenId] = true;
+    }
+
+    function withdrawNFT(uint256 _tokenId) external nonReentrant {
+        require(depositedNFTs[_tokenId], "NFT not deposited");
+        (, , uint256 weight, ) = bankReserve.bullionData(_tokenId);
+        require(weight <= goldReserve, "Insufficient gold reserves for NFT");
+        goldReserve -= weight;
+        depositedNFTs[_tokenId] = false;
     }
 }
