fb7d7c4c08d21ae803f17e3856c1f0d0ccc6746b
Robert McMenemy <55621684+Arkay92@users.noreply.github.com>
2023-06-30 20:48:29 +0100

    Create CBDC.sol

---
+++ CBDC.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+
+import "./LiquidityPool.sol";
+
+contract CBDCToken is ERC20Pausable {
+    address public immutable owner;
+    uint8 public constant DECIMALS = 18;
+    LiquidityPool public liquidityPool;
+
+    constructor(address _liquidityPool) ERC20("CBDC Token", "CBDC") {
+        owner = msg.sender;
+        liquidityPool = LiquidityPool(_liquidityPool);
+    }
+
+    function mint(address _recipient, uint256 _amount) external onlyOwner {
+        _mint(_recipient, _amount);
+    }
+
+    function decimals() public pure override returns (uint8) {
+        return DECIMALS;
+    }
+
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    function unpause() external onlyOwner {
+        _unpause();
+    }
+
+    function burn(uint256 _amount) public onlyOwner {
+        _burn(owner, _amount);
+    }
+
+    function getExchangeRate() public view returns (uint256) {
+        return liquidityPool.getExchangeRate();
+    }
+
+    // Override the transfer and transferFrom functions to adjust the token amount based on the exchange rate
+    function transfer(address _recipient, uint256 _amount) public override returns (bool) {
+        uint256 exchangedAmount = _amount * getExchangeRate();
+        return super.transfer(_recipient, exchangedAmount);
+    }
+
+    function transferFrom(address _sender, address _recipient, uint256 _amount) public override returns (bool) {
+        uint256 exchangedAmount = _amount * getExchangeRate();
+        return super.transferFrom(_sender, _recipient, exchangedAmount);
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not owner");
+        _;
+    }
+}
