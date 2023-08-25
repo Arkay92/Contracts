89cab55f2c71a3e9aad8acfc184afa02229c2cf0
Robert McMenemy <55621684+Arkay92@users.noreply.github.com>
2023-08-25 07:14:57 +0100

    Update PedersonsCommitment.sol

--- PedersonsCommitment.sol
+++ PedersonsCommitment.sol
@@ -8,33 +8,38 @@ contract PedersonsCommitment {
     }
 
     mapping(uint256 => Commitment) public commitments;
+    bytes32 public root;
 
     function commitToNumber(uint256 index, bytes32 commitmentValue) public {
         commitments[index] = Commitment(commitmentValue, true);
     }
 
+    function setRoot(bytes32 newRoot) public {
+        root = newRoot;
+    }
+
     function verifyDuplicate(uint256[] memory indices, bytes32[] memory proof) public view returns (bool) {
-        require(indices.length == proof.length, "Mismatched array lengths");
         require(indices.length > 0, "Indices array is empty");
         require(proof.length > 0, "Proof array is empty");
-        
-        bytes32 root = proof[proof.length - 1];
-        
+        require(root != bytes32(0), "Root not set");
+
+        bytes32 computedRoot = root;
+
         for (uint256 i = 0; i < indices.length; i++) {
             uint256 index = indices[i];
             Commitment storage commitment = commitments[index];
             require(commitment.isSet, "Commitment not set");
-            
+
             bytes32 commitmentValue = commitment.commitmentValue;
             bytes32 proofElement = proof[i];
-            
+
             if (i % 2 == 0) {
-                root = keccak256(abi.encodePacked(root, proofElement));
+                computedRoot = keccak256(abi.encodePacked(computedRoot, proofElement));
             } else {
-                root = keccak256(abi.encodePacked(proofElement, root));
+                computedRoot = keccak256(abi.encodePacked(proofElement, computedRoot));
             }
         }
-        
-        return root == commitmentValue; // Compare the computed root with the provided commitment value
+
+        return computedRoot == commitmentValue; // Compare the computed root with the provided commitment value
     }
 }
