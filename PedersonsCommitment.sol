74137bbf742297a940ef3c2550eeb4b85e16f531
Robert McMenemy <55621684+Arkay92@users.noreply.github.com>
2023-08-25 07:18:09 +0100

    Update PedersonsCommitment.sol

--- PedersonsCommitment.sol
+++ PedersonsCommitment.sol
@@ -10,6 +10,11 @@ contract PedersonsCommitment {
     mapping(uint256 => Commitment) public commitments;
     bytes32 public root;
 
+    modifier rootIsSet() {
+        require(root != bytes32(0), "Root not set");
+        _;
+    }
+
     function commitToNumber(uint256 index, bytes32 commitmentValue) public {
         commitments[index] = Commitment(commitmentValue, true);
     }
@@ -18,10 +23,9 @@ contract PedersonsCommitment {
         root = newRoot;
     }
 
-    function verifyDuplicate(uint256[] memory indices, bytes32[] memory proof) public view returns (bool) {
+    function verifyDuplicate(uint256[] memory indices, bytes32[] memory proof) public view rootIsSet returns (bool) {
         require(indices.length > 0, "Indices array is empty");
         require(proof.length > 0, "Proof array is empty");
-        require(root != bytes32(0), "Root not set");
 
         bytes32 computedRoot = root;
 
