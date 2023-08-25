e7ee7ecb3af8135144d7387c7916b65eaa65d8cd
Robert McMenemy <55621684+Arkay92@users.noreply.github.com>
2023-08-25 06:58:47 +0100

    Create PedersonsCommitment.sol

---
+++ PedersonsCommitment.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract PedersonsCommitment {
+    struct Commitment {
+        bytes32 commitmentValue;
+        bool isSet;
+    }
+
+    mapping(uint256 => Commitment) public commitments;
+
+    function commitToNumber(uint256 index, bytes32 commitmentValue) public {
+        commitments[index] = Commitment(commitmentValue, true);
+    }
+
+    function verifyDuplicate(uint256[] memory indices, bytes32[] memory proof) public view returns (bool) {
+        require(indices.length > 0, "Indices array is empty");
+        require(proof.length > 0, "Proof array is empty");
+        
+        bytes32 root = proof[proof.length - 1];
+        
+        for (uint256 i = 0; i < indices.length; i++) {
+            uint256 index = indices[i];
+            Commitment storage commitment = commitments[index];
+            require(commitment.isSet, "Commitment not set");
+            
+            bytes32 commitmentValue = commitment.commitmentValue;
+            bytes32 proofElement = proof[i];
+            
+            if (i % 2 == 0) {
+                root = keccak256(abi.encodePacked(root, proofElement));
+            } else {
+                root = keccak256(abi.encodePacked(proofElement, root));
+            }
+            
+            if (commitmentValue != proofElement) {
+                return false; // Invalid proof
+            }
+        }
+        
+        return root == proof[proof.length - 1];
+    }
+}
