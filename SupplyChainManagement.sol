c71171605aaf0653a6334cc966190c79e5ab3275
Robert McMenemy <55621684+Arkay92@users.noreply.github.com>
2023-10-30 09:59:23 +0000

    Update SupplyChainManagement.sol

--- SupplyChainManagement.sol
+++ SupplyChainManagement.sol
@@ -1,209 +1,255 @@
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v5.0.0) (access/AccessControl.sol)
-
 pragma solidity ^0.8.20;
 
-import {IAccessControl} from "./IAccessControl.sol";
-import {Context} from "../utils/Context.sol";
-import {ERC165} from "../utils/introspection/ERC165.sol";
+import "https://github.com/chiru-labs/PBT/blob/main/src/IPBT.sol";
+import "https://github.com/chiru-labs/PBT/blob/main/src/ERC721ReadOnly.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+error InvalidSignature();
+error NoMintedTokenForChip();
+error NoMappedTokenForChip();
+error ArrayLengthMismatch();
+error SeedingChipDataForExistingToken();
+error UpdatingChipForUnsetChipMapping();
+error InvalidBlockNumber();
+error BlockNumberTooOld();
 
 /**
- * @dev Contract module that allows children to implement role-based access
- * control mechanisms. This is a lightweight version that doesn't allow enumerating role
- * members except through off-chain means by accessing the contract event logs. Some
- * applications may benefit from on-chain enumerability, for those cases see
- * {AccessControlEnumerable}.
- *
- * Roles are referred to by their `bytes32` identifier. These should be exposed
- * in the external API and be unique. The best way to achieve this is by
- * using `public constant` hash digests:
- *
- * ```solidity
- * bytes32 public constant MY_ROLE = keccak256("MY_ROLE");
- * ```
- *
- * Roles can be used to represent a set of permissions. To restrict access to a
- * function call, use {hasRole}:
- *
- * ```solidity
- * function foo() public {
- *     require(hasRole(MY_ROLE, msg.sender));
- *     ...
- * }
- * ```
- *
- * Roles can be granted and revoked dynamically via the {grantRole} and
- * {revokeRole} functions. Each role has an associated admin role, and only
- * accounts that have a role's admin role can call {grantRole} and {revokeRole}.
- *
- * By default, the admin role for all roles is `DEFAULT_ADMIN_ROLE`, which means
- * that only accounts with this role will be able to grant or revoke other
- * roles. More complex role relationships can be created by using
- * {_setRoleAdmin}.
- *
- * WARNING: The `DEFAULT_ADMIN_ROLE` is also its own admin: it has permission to
- * grant and revoke this role. Extra precautions should be taken to secure
- * accounts that have been granted it. We recommend using {AccessControlDefaultAdminRules}
- * to enforce additional security measures for this role.
+ * Implementation of PBT where all chipAddress->tokenIds are preset in the contract by the contract owner.
  */
-abstract contract AccessControl is Context, IAccessControl, ERC165 {
-    struct RoleData {
-        mapping(address account => bool) hasRole;
-        bytes32 adminRole;
+contract PBTSimple is ERC721ReadOnly, IPBT, AccessControl {
+    using ECDSA for bytes32;
+
+    address owner;
+
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+    bytes32 public constant DISTRIBUTOR_ROLE = keccak256("DISTRIBUTOR_ROLE");
+    bytes32 public constant SUPPLIER_ROLE = keccak256("SUPPLIER_ROLE");
+    bytes32 public constant CONSUMER_ROLE = keccak256("CONSUMER_ROLE");
+
+    struct TokenData {
+        uint256 tokenId;
+        address chipAddress;
+        bool set;
     }
 
-    mapping(bytes32 role => RoleData) private _roles;
+    /**
+     * Mapping from chipAddress to TokenData
+     */
+    mapping(address => TokenData) _tokenDatas;
 
-    bytes32 public constant DEFAULT_ADMIN_ROLE = 0x00;
+    constructor(string memory name_, string memory symbol_) ERC721ReadOnly(name_, symbol_) {
+        owner = msg.sender;
+    }
 
     /**
-     * @dev Modifier that checks that an account has a specific role. Reverts
-     * with an {AccessControlUnauthorizedAccount} error including the required role.
+     * Modifiers
      */
-    modifier onlyRole(bytes32 role) {
-        _checkRole(role);
+    modifier onlyDistributor() {
+        require(hasRole(DISTRIBUTOR_ROLE, _msgSender()), "Must have DISTRIBUTOR_ROLE");
         _;
+    }
+
+    modifier onlySupplier() {
+        require(hasRole(SUPPLIER_ROLE, _msgSender()), "Must have SUPPLIER_ROLE");
+        _;
+    }
+
+    modifier onlyConsumer() {
+        require(hasRole(CONSUMER_ROLE, _msgSender()), "Must have CONSUMER_ROLE");
+        _;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call");
+        _;
+    }
+
+    // Should only be called for tokenIds that have not yet been minted
+    // If the tokenId has already been minted, use _updateChips instead
+    // TODO: consider preventing multiple chip addresses mapping to the same tokenId (store a tokenId->chip mapping)
+    function _seedChipToTokenMapping(address[] memory chipAddresses, uint256[] memory tokenIds) internal onlySupplier {
+        _seedChipToTokenMapping(chipAddresses, tokenIds, true);
+    }
+
+    function _seedChipToTokenMapping(
+        address[] memory chipAddresses,
+        uint256[] memory tokenIds,
+        bool throwIfTokenAlreadyMinted
+    ) internal onlySupplier {
+        uint256 tokenIdsLength = tokenIds.length;
+        if (tokenIdsLength != chipAddresses.length) {
+            revert ArrayLengthMismatch();
+        }
+        for (uint256 i = 0; i < tokenIdsLength; ++i) {
+            address chipAddress = chipAddresses[i];
+            uint256 tokenId = tokenIds[i];
+            if (throwIfTokenAlreadyMinted && _exists(tokenId)) {
+                revert SeedingChipDataForExistingToken();
+            }
+            _tokenDatas[chipAddress] = TokenData(tokenId, chipAddress, true);
+        }
+    }
+
+    // Should only be called for tokenIds that have been minted
+    // If the tokenId hasn't been minted yet, use _seedChipToTokenMapping instead
+    // Should only be used and called with care and rails to avoid a centralized entity swapping out valid chips.
+    // TODO: consider preventing multiple chip addresses mapping to the same tokenId (store a tokenId->chip mapping)
+    function _updateChips(address[] calldata chipAddressesOld, address[] calldata chipAddressesNew) internal onlySupplier {
+        if (chipAddressesOld.length != chipAddressesNew.length) {
+            revert ArrayLengthMismatch();
+        }
+        for (uint256 i = 0; i < chipAddressesOld.length; ++i) {
+            address oldChipAddress = chipAddressesOld[i];
+            TokenData memory oldTokenData = _tokenDatas[oldChipAddress];
+            if (!oldTokenData.set) {
+                revert UpdatingChipForUnsetChipMapping();
+            }
+            address newChipAddress = chipAddressesNew[i];
+            uint256 tokenId = oldTokenData.tokenId;
+            _tokenDatas[newChipAddress] = TokenData(tokenId, newChipAddress, true);
+            if (_exists(tokenId)) {
+                emit PBTChipRemapping(tokenId, oldChipAddress, newChipAddress);
+            }
+            delete _tokenDatas[oldChipAddress];
+        }
+    }
+
+    function tokenIdFor(address chipAddress) external view override returns (uint256) {
+        uint256 tokenId = tokenIdMappedFor(chipAddress);
+        if (!_exists(tokenId)) {
+            revert NoMintedTokenForChip();
+        }
+        return tokenId;
+    }
+
+    function tokenIdMappedFor(address chipAddress) public view returns (uint256) {
+        if (!_tokenDatas[chipAddress].set) {
+            revert NoMappedTokenForChip();
+        }
+        return _tokenDatas[chipAddress].tokenId;
+    }
+
+    // Returns true if the signer of the signature of the payload is the chip for the token id
+    function isChipSignatureForToken(uint256 tokenId, bytes memory payload, bytes memory signature)
+        public
+        view
+        override
+        returns (bool)
+    {
+        if (!_exists(tokenId)) {
+            revert NoMintedTokenForChip();
+        }
+        bytes32 signedHash = keccak256(payload).toEthSignedMessageHash();
+        address chipAddr = signedHash.recover(signature);
+        return _tokenDatas[chipAddr].set && _tokenDatas[chipAddr].tokenId == tokenId;
+    }
+
+    //
+    // Parameters:
+    //    to: the address of the new owner
+    //    signatureFromChip: signature(receivingAddress + recentBlockhash), signed by an approved chip
+    //
+    // Contract should check that (1) recentBlockhash is a recent blockhash, (2) receivingAddress === to, and (3) the signing chip is allowlisted.
+    function _mintTokenWithChip(bytes calldata signatureFromChip, uint256 blockNumberUsedInSig)
+        internal
+        returns (uint256)
+    {
+        TokenData memory tokenData = _getTokenDataForChipSignature(signatureFromChip, blockNumberUsedInSig);
+        uint256 tokenId = tokenData.tokenId;
+        _mint(_msgSender(), tokenId);
+        emit PBTMint(tokenId, tokenData.chipAddress);
+        return tokenId;
+    }
+
+    function transferTokenWithChip(bytes calldata signatureFromChip, uint256 blockNumberUsedInSig) public override {
+        _transferTokenWithChip(signatureFromChip, blockNumberUsedInSig, false);
+    }
+
+    function transferTokenWithChip(
+        bytes calldata signatureFromChip,
+        uint256 blockNumberUsedInSig,
+        bool useSafeTransferFrom
+    ) public override onlyDistributor {
+        _transferTokenWithChip(signatureFromChip, blockNumberUsedInSig, useSafeTransferFrom);
+    }
+
+    function transferTokenToConsumer(uint256 tokenId) public onlyConsumer {
+        // Ensure the caller is a consumer and can transfer the token to themselves
+        require(ownerOf(tokenId) == _msgSender(), "Caller is not the owner of the token");
+        _transfer(ownerOf(tokenId), _msgSender(), tokenId);
+    }
+
+    function _transferTokenWithChip(
+        bytes calldata signatureFromChip,
+        uint256 blockNumberUsedInSig,
+        bool useSafeTransferFrom
+    ) internal virtual {
+        uint256 tokenId = _getTokenDataForChipSignature(signatureFromChip, blockNumberUsedInSig).tokenId;
+        if (useSafeTransferFrom) {
+            _safeTransfer(ownerOf(tokenId), _msgSender(), tokenId, "");
+        } else {
+            _transfer(ownerOf(tokenId), _msgSender(), tokenId);
+        }
+    }
+
+    function _getTokenDataForChipSignature(bytes calldata signatureFromChip, uint256 blockNumberUsedInSig)
+        internal
+        view
+        returns (TokenData memory)
+    {
+        // The blockNumberUsedInSig must be in a previous block because the blockhash of the current
+        // block does not exist yet.
+        if (block.number <= blockNumberUsedInSig) {
+            revert InvalidBlockNumber();
+        }
+
+        unchecked {
+            if (block.number - blockNumberUsedInSig > getMaxBlockhashValidWindow()) {
+                revert BlockNumberTooOld();
+            }
+        }
+
+        bytes32 blockHash = blockhash(blockNumberUsedInSig);
+        bytes32 signedHash = keccak256(abi.encodePacked(_msgSender(), blockHash)).toEthSignedMessageHash();
+        address chipAddr = signedHash.recover(signatureFromChip);
+
+        TokenData memory tokenData = _tokenDatas[chipAddr];
+        if (tokenData.set) {
+            return tokenData;
+        }
+        revert InvalidSignature();
+    }
+
+    function getMaxBlockhashValidWindow() public pure virtual returns (uint256) {
+        return 100;
     }
 
     /**
      * @dev See {IERC165-supportsInterface}.
      */
-    function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
-        return interfaceId == type(IAccessControl).interfaceId || super.supportsInterface(interfaceId);
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, AccessControl) returns (bool) {
+        return interfaceId == type(IPBT).interfaceId || super.supportsInterface(interfaceId);
     }
 
     /**
-     * @dev Returns `true` if `account` has been granted `role`.
+     * Role based functions
      */
-    function hasRole(bytes32 role, address account) public view virtual returns (bool) {
-        return _roles[role].hasRole[account];
+
+    function grantAdminRole(address account) public onlyOwner {
+        grantRole(ADMIN_ROLE, account);
     }
 
-    /**
-     * @dev Reverts with an {AccessControlUnauthorizedAccount} error if `_msgSender()`
-     * is missing `role`. Overriding this function changes the behavior of the {onlyRole} modifier.
-     */
-    function _checkRole(bytes32 role) internal view virtual {
-        _checkRole(role, _msgSender());
+    function grantDistributorRole(address account) public onlyRole(ADMIN_ROLE) {
+        grantRole(DISTRIBUTOR_ROLE, account);
     }
 
-    /**
-     * @dev Reverts with an {AccessControlUnauthorizedAccount} error if `account`
-     * is missing `role`.
-     */
-    function _checkRole(bytes32 role, address account) internal view virtual {
-        if (!hasRole(role, account)) {
-            revert AccessControlUnauthorizedAccount(account, role);
-        }
+    function grantSupplierRole(address account) public onlyRole(ADMIN_ROLE) {
+        grantRole(SUPPLIER_ROLE, account);
     }
 
-    /**
-     * @dev Returns the admin role that controls `role`. See {grantRole} and
-     * {revokeRole}.
-     *
-     * To change a role's admin, use {_setRoleAdmin}.
-     */
-    function getRoleAdmin(bytes32 role) public view virtual returns (bytes32) {
-        return _roles[role].adminRole;
-    }
-
-    /**
-     * @dev Grants `role` to `account`.
-     *
-     * If `account` had not been already granted `role`, emits a {RoleGranted}
-     * event.
-     *
-     * Requirements:
-     *
-     * - the caller must have ``role``'s admin role.
-     *
-     * May emit a {RoleGranted} event.
-     */
-    function grantRole(bytes32 role, address account) public virtual onlyRole(getRoleAdmin(role)) {
-        _grantRole(role, account);
-    }
-
-    /**
-     * @dev Revokes `role` from `account`.
-     *
-     * If `account` had been granted `role`, emits a {RoleRevoked} event.
-     *
-     * Requirements:
-     *
-     * - the caller must have ``role``'s admin role.
-     *
-     * May emit a {RoleRevoked} event.
-     */
-    function revokeRole(bytes32 role, address account) public virtual onlyRole(getRoleAdmin(role)) {
-        _revokeRole(role, account);
-    }
-
-    /**
-     * @dev Revokes `role` from the calling account.
-     *
-     * Roles are often managed via {grantRole} and {revokeRole}: this function's
-     * purpose is to provide a mechanism for accounts to lose their privileges
-     * if they are compromised (such as when a trusted device is misplaced).
-     *
-     * If the calling account had been revoked `role`, emits a {RoleRevoked}
-     * event.
-     *
-     * Requirements:
-     *
-     * - the caller must be `callerConfirmation`.
-     *
-     * May emit a {RoleRevoked} event.
-     */
-    function renounceRole(bytes32 role, address callerConfirmation) public virtual {
-        if (callerConfirmation != _msgSender()) {
-            revert AccessControlBadConfirmation();
-        }
-
-        _revokeRole(role, callerConfirmation);
-    }
-
-    /**
-     * @dev Sets `adminRole` as ``role``'s admin role.
-     *
-     * Emits a {RoleAdminChanged} event.
-     */
-    function _setRoleAdmin(bytes32 role, bytes32 adminRole) internal virtual {
-        bytes32 previousAdminRole = getRoleAdmin(role);
-        _roles[role].adminRole = adminRole;
-        emit RoleAdminChanged(role, previousAdminRole, adminRole);
-    }
-
-    /**
-     * @dev Attempts to grant `role` to `account` and returns a boolean indicating if `role` was granted.
-     *
-     * Internal function without access restriction.
-     *
-     * May emit a {RoleGranted} event.
-     */
-    function _grantRole(bytes32 role, address account) internal virtual returns (bool) {
-        if (!hasRole(role, account)) {
-            _roles[role].hasRole[account] = true;
-            emit RoleGranted(role, account, _msgSender());
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    /**
-     * @dev Attempts to revoke `role` to `account` and returns a boolean indicating if `role` was revoked.
-     *
-     * Internal function without access restriction.
-     *
-     * May emit a {RoleRevoked} event.
-     */
-    function _revokeRole(bytes32 role, address account) internal virtual returns (bool) {
-        if (hasRole(role, account)) {
-            _roles[role].hasRole[account] = false;
-            emit RoleRevoked(role, account, _msgSender());
-            return true;
-        } else {
-            return false;
-        }
+    function grantConsumerRole(address account) public onlyRole(ADMIN_ROLE) {
+        grantRole(CONSUMER_ROLE, account);
     }
 }
